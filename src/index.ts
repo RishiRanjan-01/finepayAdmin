d3521203c1e83c0f1ee00b2bafcac611f0b32043
RishiRanjan-01 <rishibhumi000@gmail.com>
2024-07-09 01:09:04 +0530

    mendatory package included

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
